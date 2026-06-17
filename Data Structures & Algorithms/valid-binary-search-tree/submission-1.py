eff2a63532107267c2624f7e084c363ef4833a58
naraharigh <narahari_sy@yahoo.co.in>
2026-06-17 13:41:51 -0400

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode], low=float('-inf'), high=float('inf')) -> bool:
+        if not root:
+            return True
+        
+        if not (low < root.val < high):
+            return False
+            
+        return self.isValidBST(root.left, low, root.val) and self.isValidBST(root.right, root.val, high)
