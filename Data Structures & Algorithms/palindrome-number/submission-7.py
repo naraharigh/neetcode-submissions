f8822a83887b3d4bceab4eaae832fd3af4164eb5
naraharigh <narahari_sy@yahoo.co.in>
2026-07-06 09:19:18 -0400

    Add: palindrome-number - submission-7

---
+++ Data Structures & Algorithms/palindrome-number/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        number =abs(x)
+        i = 0
+        result = 0
+        if  x < 0:
+            return False
+        if x < 10 :
+            return True
+        while(number!= 0):
+            digit = number%10
+            result = result * 10 + digit
+            number = number//10
+
+        print (result)    
+        if result == x :
+            return True
+        return False
