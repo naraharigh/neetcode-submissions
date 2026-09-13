c1c1531f9d366d5dceca050e93c4a171b88365e3
naraharigh <narahari_sy@yahoo.co.in>
2026-09-13 05:50:00 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        Array.Sort(nums);
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if(nums[i] ==  nums[i-1])
+            {
+                return true;
+                break;
+            }
+
+        }
+        
+        return false;
+    }
+}
